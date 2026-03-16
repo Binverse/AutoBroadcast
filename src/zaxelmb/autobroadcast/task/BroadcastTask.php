38038f7c65f7de55c21b247b78f9e46e11949b5b
Axel <Axelpxrxz2000@gmail.com>
2026-03-16 13:24:42 -0600

    Add files via upload

---
+++ src/zaxelmb/autobroadcast/task/BroadcastTask.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace zaxelmb\autobroadcast\task;
+
+use zaxelmb\autobroadcast\Loader;
+use pocketmine\scheduler\Task;
+
+class BroadcastTask extends Task {
+    
+    private $plugin;
+    
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
+    }
+    
+    public function onRun(): void {
+        $manager = $this->plugin->getBroadcastManager();
+        $manager->sendBroadcast();
+    }
+}
